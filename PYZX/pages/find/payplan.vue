1bd84531d377ec3ab26562802fbfda23abf8cf2a
刘启明 <455043818@qq.com>
2020-01-13 16:16:11 +0800

    还款计划页面

--- PYZX/pages/find/payplan.vue
+++ PYZX/pages/find/payplan.vue
@@ -12,7 +12,44 @@
 			</view>
 		</scroll-view>
 		<block v-if="TabCur==0">
-			111111
+			<view class="cu-bar bg-white">
+				<view class="action">
+					<text class="cuIcon-title text-green"></text>
+					<text>信用卡还款</text>
+				</view>
+				<view class="action">
+					<button class="cu-btn bg-green shadow-blur round">新增</button>
+				</view>
+			</view>
+			<checkbox-group class="block" @change="CheckboxChange">
+				<view class="cu-form-group">
+					<view class="title">招商信用卡</view><view class="title">5200</view><view class="fontsize">每月5日</view>
+					<checkbox class='round' :class="checkbox[1].checked?'checked':''" :checked="checkbox[1].checked?true:false" value="B"></checkbox>
+				</view>
+				<view class="cu-form-group">
+					<view class="title">建行信用卡</view><view class="title">4200</view><view class="fontsize">每月5日</view>
+					<checkbox class='round blue' :class="checkbox[2].checked?'checked':''" :checked="checkbox[2].checked?true:false"
+					 value="C"></checkbox>
+				</view>
+			</checkbox-group>
+			<view class="cu-bar bg-white">
+				<view class="action">
+					<text class="cuIcon-title text-blue"></text>
+					<text>借款还款</text>
+				</view>
+				<view class="action">
+					<button class="cu-btn bg-blue shadow-blur round">新增</button>
+				</view>
+			</view>
+			<view class="cu-form-group">
+				<view class="title">彭冰光</view><view class="title">60000</view><view class="fontsize">10月5日</view>
+				<checkbox class='round' :class="checkbox[1].checked?'checked':''" :checked="checkbox[1].checked?true:false" value="B"></checkbox>
+			</view>
+			<view class="cu-form-group">
+				<view class="title">钟礼军</view><view class="title">4200</view><view class="fontsize">10月5日</view>
+				<checkbox class='round blue' :class="checkbox[2].checked?'checked':''" :checked="checkbox[2].checked?true:false"
+				 value="C"></checkbox>
+			</view>
 		</block>
 		<block v-if="TabCur==1">
 			222222
@@ -37,11 +74,26 @@
 				this.TabCur = e.currentTarget.dataset.id;
 				this['current_content']=this.TabCur;
 				this.scrollLeft = (e.currentTarget.dataset.id - 1) * 60
+			},
+			CheckboxChange(e) {
+				var items = this.checkbox,
+					values = e.detail.value;
+				for (var i = 0, lenI = items.length; i < lenI; ++i) {
+					items[i].checked = false;
+					for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+						if (items[i].value == values[j]) {
+							items[i].checked = true;
+							break
+						}
+					}
+				}
 			}
 		}
 	}
 </script>
 
 <style>
-
+	.fontsize{
+		font-size: 18upx;
+	}
 </style>
